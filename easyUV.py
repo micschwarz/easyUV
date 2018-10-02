0ea6f005aa51de9e71339be0e18309e10de402a9
Michael Schwarz <089michael@gmail.com>
2018-10-02 21:38:04 +0200

    Update for Blender 2.8

--- easyUV.py
+++ easyUV.py
@@ -1,26 +1,27 @@
-#-------------------------------------------------------------------------------------------
-#Plugin Infos
-bl_info={
-    "name" : "easyUV",
-    "author" : "Michael S.",
-    "version" : (0,1),
-    "blender" : (2, 77, 0),
-    "location" : "Properties > Material",
-    "description" : "easy UV making",
-    "category" : "Material"
+# -------------------------------------------------------------------------------------------
+# Plugin Infos
+bl_info = {
+    "name": "easyUV",
+    "author": "Michael S.",
+    "version": (0, 1),
+    "blender": (2, 77, 0),
+    "location": "Properties > Material",
+    "description": "easy UV making",
+    "category": "Material"
 }
-#-------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------
 import bpy
 from bpy.props import *
 
-#Attribute
+# Attribute
 atr = bpy.types.Scene
 
-#atr.name = StringProperty(name = "name", default = "lol", description = "text")
 
-#shared functions
+# atr.name = StringProperty(name = "name", default = "lol", description = "text")
+
+# shared functions
 class sf(bpy.types.Operator):
-    #edit mode: m == True -> select all verts
+    # edit mode: m == True -> select all verts
     def edit(m):
         sop = bpy.ops
         sop.object.editmode_toggle()
@@ -29,82 +30,86 @@ class sf(bpy.types.Operator):
             sop.mesh.select_all(action='TOGGLE')
         return {'FINISHED'}
 
-#Create UI: Tools Tab
-class window (bpy.types.Panel):
+
+# Create UI: Tools Tab
+class window(bpy.types.Panel):
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
     bl_context = "material"
     bl_label = "easy UV"
     bl_category = "easyUV"
-    
-    
+
     def draw(self, context):
         layout = self.layout
-        row = layout.row(align = True)
-        row.operator("uv.cube",text = "Cube", icon="MESH_CUBE")
-        row.operator("uv.sphere",text = "Sphere", icon="MATSPHERE")
-        row.operator("uv.plane",text = "Plane", icon="MESH_PLANE")
-   
-        
-#Create UI: Buttons
+        row = layout.row(align=True)
+        row.operator("uv.cube", text="Cube", icon="MESH_CUBE")
+        row.operator("uv.sphere", text="Sphere", icon="MATSPHERE")
+        row.operator("uv.plane", text="Plane", icon="MESH_PLANE")
 
-#UV Cube
+
+# Create UI: Buttons
+
+# UV Cube
 class OBJECT_OT_buttonCube(bpy.types.Operator):
     bl_label = "Cube"
     bl_idname = "uv.cube"
     bl_description = "Create Cube UV"
-    
-    def execute(self, context):       
-        #Shorts
+
+    def execute(self, context):
+        # Shorts
         p = print
         scene = bpy.context.scene
-        sop = bpy.ops        
-    
-        #Uv Map
+        sop = bpy.ops
+
+        # Uv Map
         sf.edit(True)
         sop.uv.cube_project()
         sf.edit(False)
-        return{'FINISHED'}
-    
-#UV Sphere
+        return {'FINISHED'}
+
+
+# UV Sphere
 class OBJECT_OT_buttonSphere(bpy.types.Operator):
     bl_label = "Cube"
     bl_idname = "uv.sphere"
     bl_description = "Create Sphere UV"
-    
-    def execute(self, context):       
-        #Shorts
+
+    def execute(self, context):
+        # Shorts
         p = print
         scene = bpy.context.scene
-        sop = bpy.ops        
-    
-        #Uv Map
+        sop = bpy.ops
+
+        # Uv Map
         sf.edit(True)
         sop.uv.sphere_project()
         sf.edit(False)
-        return{'FINISHED'}
-    
-#UV Plane
+        return {'FINISHED'}
+
+
+# UV Plane
 class OBJECT_OT_buttonPlane(bpy.types.Operator):
     bl_label = "Cube"
     bl_idname = "uv.plane"
     bl_description = "Create Plane UV"
-    
-    def execute(self, context):       
-        #Shorts
+
+    def execute(self, context):
+        # Shorts
         p = print
         scene = bpy.context.scene
-        sop = bpy.ops        
-    
-        #Uv Map
+        sop = bpy.ops
+
+        # Uv Map
         sf.edit(True)
         sop.uv.unwrap(method='ANGLE_BASED', margin=0.001)
         sf.edit(False)
-        return{'FINISHED'}
-    
-        
+        return {'FINISHED'}
+
+
 def register(): bpy.utils.register_module(__name__)
-    
+
+
 def unregister(): bpy.utils.unregister_module(__name__)
-    
+
+
 if __name__ == "__main__": register()
